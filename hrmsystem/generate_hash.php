965bf03c897882b32cbd1d9f7f85a5cb38df0060
baygor12 <feitanlasco@gmail.com>
2024-09-17 17:19:27 +0800

    Update generate_hash.php

--- hrmsystem/generate_hash.php
+++ hrmsystem/generate_hash.php
@@ -1,10 +1,7 @@
 <?php
-// Replace 'your_password' with the actual password you want to hash
-$password = 'your_password';
+$password = 'admin123'; // The plain text password
+$hashed_password = password_hash($password, PASSWORD_DEFAULT); // Generate the hash
 
-// Generate the hashed password
-$hashed_password = password_hash($password, PASSWORD_DEFAULT);
-
-// Print the hashed password
-echo $hashed_password;
+echo "Plain Password: " . $password . "<br>";
+echo "Hashed Password: " . $hashed_password;
 ?>
